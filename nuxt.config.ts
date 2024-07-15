34d18c2fb350c523814f8d4d8d03d08b1bd6f7c5
andreslqr <me@andreslopez.com.mx>
2024-07-15 11:27:11 -0600

    added meta for noindex

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxt/content'],
-  routeRules: {
-    '/': { prerender: true }
-  }
+    app: {
+        head: {
+            meta: [
+                {
+                    name: 'robots',
+                    content: 'noindex'
+                },
+
+            ]
+        }
+    },
+    devtools: { enabled: true },
+    modules: ['@nuxt/content'],
+    routeRules: {
+        '/': { prerender: true }
+    }
 })
